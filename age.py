f8ee3ef3663bdfaa0f2470da9aa0fbc8e49e09b0
Harleen Dhatt <111666380+h-dhatt@users.noreply.github.com>
2022-08-29 06:10:55 -0400

    Update age.py

--- age.py
+++ age.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 import time
 from calendar import isleap
 
-# judge the leap year
 def judge_leap_year(year):
     if isleap(year):
         return True
@@ -10,7 +8,6 @@ def judge_leap_year(year):
         return False
 
 
-# returns the number of days in each month
 def month_days(month, leap_year):
     if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
